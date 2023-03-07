d62610599dfb5098a29b05008c1b052659e3eaf2
Oleksandr Hrin <1nutak1@gmail.com>
2023-03-07 11:42:53 +0200

    added universal locations handler

--- locations/router.js
+++ locations/router.js
@@ -5,9 +5,25 @@ import { LocationService } from "./index.js";
 const locationsRouter = express.Router();
 
 locationsRouter.get("/lisove", async (req, res) => {
-  const response = await LocationService.getDataByLocation(6499, 1);
+  console.log("Getting electricity schedule for Lisove");
+  res.send(await LocationService.getDataByLocation(6499, 1));
+});
 
-  res.send(response);
+locationsRouter.get("/locations", async (req, res) => {
+  const { street, house, flat } = req.query;
+  console.log(
+    `Getting electricity schedule for Location: Street:${street}, House: ${house}, Flat: ${flat}`
+  );
+  if (!street && !house)
+    res
+      .status(400)
+      .send({
+        error:
+          "Please provide a street and house in a number format, can be taken on https://cabinet.cherkasyoblenergo.com/gv.php in DevTools",
+      });
+  const response = res.send(
+    await LocationService.getDataByLocation(street, house, flat)
+  );
 });
 
 locationsRouter.get("/", async (req, res) => {

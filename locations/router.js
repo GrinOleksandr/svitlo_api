23d0ef15f0c71d5d6108239493adb81d3e50a7f9
Oleksandr Hrin <1nutak1@gmail.com>
2023-03-07 11:19:20 +0200

    testing deploy

--- locations/router.js
+++ locations/router.js
@@ -5,7 +5,7 @@ import { LocationService } from "./index.js";
 const locationsRouter = express.Router();
 
 locationsRouter.get("/lisove", async (req, res) => {
-  const response = await LocationService.getDataByLocation(6499, 1);
+  const response = await LocationService.getDataByLocation(6499, 1);//
 
   res.send(response);
 });

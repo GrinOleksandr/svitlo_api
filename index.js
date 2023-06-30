5ff69f8acc30525d4620ba0dbb9d2e086441f8e1
Oleksandr Hrin <1nutak1@gmail.com>
2023-06-30 10:52:50 +0300

    add static files server

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import { locationsRouter } from "./locations/index.js";
+import cors from "cors"
 
 const port = 8000;
 const app = express();
 
+app.use(cors)
 app.use(locationsRouter);
+app.use(express.static('public'));
 
 app.listen(port, () => {
   console.log(`Server started at http://localhost:${port}`);
